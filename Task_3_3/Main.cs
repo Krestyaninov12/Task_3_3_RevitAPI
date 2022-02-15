3092c1ca86bb85ea924d1a0ae54f4ccf3a751990
Krestyaninov12 <kreda@pnpe.ru>
2022-02-16 00:48:57 +0300

    Добавьте файлы проекта.

---
+++ Task_3_3/Main.cs
@@ -0,0 +1,87 @@
+﻿using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Plumbing;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_3
+{//
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            //Выбор объекта с фильтром для труб
+            var selectedRef = uidoc.Selection.PickObject(ObjectType.Element, new PipeFilter(), "Выберите трубы");
+            var selectedElement = doc.GetElement(selectedRef);
+
+            //Добавление параметра "Длина с запасом"
+            var categorySet = new CategorySet();
+            categorySet.Insert(Category.GetCategory(doc, BuiltInCategory.OST_PipeCurves));
+
+            using (Transaction ts = new Transaction(doc, "Add parameter"))
+            {
+                ts.Start();
+                CreateSharedParameter(uiapp.Application, doc, "Длина с запасом", categorySet, BuiltInParameterGroup.PG_GEOMETRY, true);
+                ts.Commit();
+            }
+
+            //Получение значения параметра "Длина"
+            Parameter lenghtParameter = selectedElement.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH);
+
+            if (lenghtParameter.StorageType == StorageType.Double)
+            {
+                var L = lenghtParameter.AsDouble();
+
+                //Установка значения в параметр "Длина с запасом"
+                using (Transaction ts = new Transaction(doc, "Set parameters"))
+                {
+                    ts.Start();
+                    Parameter stockLenghtParameter = selectedElement.LookupParameter("Длина с запасом");
+                    stockLenghtParameter.Set(L * 1.1 / 1000);
+                    ts.Commit();
+                }
+            }
+            return Result.Succeeded;
+        }
+
+        private void CreateSharedParameter(Application application,
+            Document doc, string parameterName, CategorySet categorySet,
+            BuiltInParameterGroup builtInParameterGroup, bool isInstance)
+        {
+            DefinitionFile definitionFile = application.OpenSharedParameterFile();
+            if (definitionFile == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден файл общих параметров");
+                return;
+            }
+
+            Definition definition = definitionFile.Groups
+                .SelectMany(group => group.Definitions)
+                .FirstOrDefault(def => def.Name.Equals(parameterName));
+            if (definition == null)
+            {
+                TaskDialog.Show("Ошибка", "Не найден указанный параметр");
+                return;
+            }
+
+            Binding binding = application.Create.NewTypeBinding(categorySet);
+            if (isInstance)
+                binding = application.Create.NewInstanceBinding(categorySet);
+
+            BindingMap map = doc.ParameterBindings;
+            map.Insert(definition, binding, builtInParameterGroup);
+        }
+
+    }
+}
